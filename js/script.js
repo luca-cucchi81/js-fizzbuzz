8282ffe6124881de0973a89a43c86ec7c8bc4f52
Luca <lu.cucchi@gmail.com>
2020-02-20 15:35:04 +0100

    Avvio esercizio + termine esercizio (manca css)

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*SCOPO DEL GIOCO: Fizz Buzz test:
+Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
+
+//imposto un ciclo che mi prenda tutti i numeri da 0 a 100 compreso;
+//imposto condizionali che mi permettano di operare sui multipli di 3 e sui multipli di 5
+// stampo risultati in console e html
+
+for (var numero = 1; numero <= 100; numero++) {
+    if ((numero % 3 == 0) && (numero % 5 == 0)) {
+        console.log('FizzBuzz');
+        document.getElementById('lista-pazzi').innerHTML+= '<li>FizzBuzz</li>';
+    } else if (numero % 3 == 0) {
+        console.log('Fizz');
+        document.getElementById('lista-pazzi').innerHTML+= '<li>Fizz</li>';
+    } else if (numero % 5 == 0) {
+        console.log('Buzz');
+        document.getElementById('lista-pazzi').innerHTML+= '<li>Buzz</li>';
+    } else {
+        console.log(numero);
+        document.getElementById('lista-pazzi').innerHTML+= '<li>' + numero + '</li>';
+    }
+}
