c581611b7322d59f1e22a3001c28cbb3c71e9c49
Luca <lu.cucchi@gmail.com>
2020-02-20 16:38:44 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*SCOPO DEL GIOCO: Fizz Buzz test:
 Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
-//imposto un ciclo che mi prenda tutti i numeri da 0 a 100 compreso;
+//imposto un ciclo che mi prenda tutti i numeri da 1 a 100 compreso;
 //imposto condizionali che mi permettano di operare sui multipli di 3 e sui multipli di 5
 // stampo risultati in console e html
 
